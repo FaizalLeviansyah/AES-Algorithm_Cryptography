c18aa7c34440c27ddfb925e97fa4f6dd88fec280
Faizal <91838778+FaizalLeviansyah@users.noreply.github.com>
2025-05-12 23:27:51 +0700

    change the style

--- dashboard/decrypt-file.php
+++ dashboard/decrypt-file.php
@@ -131,7 +131,7 @@ $data = mysqli_fetch_array($query);
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="logo-pro">
-                        <a href="index.html"><img class="main-logo" src="img/logo/logo.png" alt="" /></a>
+                        <a href="index.html"><img class="main-logo" src="img/logo/logo.png" alt="" onerror="this.style.display='none';" /></a>
                     </div>
                 </div>
             </div>
