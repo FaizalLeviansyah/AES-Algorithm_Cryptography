b223a3c4599b96c6a36508c76c7bd6804156c8d1
Faizal <91838778+FaizalLeviansyah@users.noreply.github.com>
2025-06-23 03:11:29 +0700

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                        <x-nav-link :href="route('file.encrypt.create')" :active="request()->routeIs('file.encrypt.create')">
+                        {{ __('Enkripsi') }}
+                    </x-nav-link>
                 </div>
+
             </div>
 
             <!-- Settings Dropdown -->
