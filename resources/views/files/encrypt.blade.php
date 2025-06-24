b9f37525175ae620ceffc4b08d219303528c3916
Faizal <91838778+FaizalLeviansyah@users.noreply.github.com>
2025-06-24 18:32:43 +0700

    Modify the default of encrypt.blade.php

--- resources/views/files/encrypt.blade.php
+++ resources/views/files/encrypt.blade.php
@@ -36,7 +36,7 @@
                             <x-input-label for="bit" :value="__('Pilih Ukuran AES')" />
                             <select name="bit" id="bit" class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
                                 <option value="128">AES-128</option>
-                                <option value="256">AES-256</option>
+                                <option value="256" selected>AES-256</option>
                             </select>
                             <x-input-error :messages="$errors->get('bit')" class="mt-2" />
                         </div>
